513727c831e6248b8dc2748c361ab8d37032ab89
JuanPabloArmas <jparmas@uc.cl>
2023-02-18 18:23:53 -0500

    Updated 01

--- askgpt/main.py
+++ askgpt/main.py
@@ -1,34 +1,16 @@
-# If you don't have python installed, or don't want to install 
-# any required libraries (like openai and questionary)
-# you can do this workshop online from 
-# https://replit.com/@sus118/AskGPT-CLI-Tool#INSTRUCTIONS-README.MD
-
 import openai
-import yaml
-import string
-import questionary
+import os
 
-# Set OpenAI API key
-openai.api_key_path = 'apikey.file'
+# Set up the OpenAI API key
+openai.api_key = "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"
 
+# Call the OpenAI API to generate text
+response = openai.Image.create(
+    prompt="A guy showing the program to his girlfriend at the beach",
+    n=1,
+    size="1024x1024"
+)
+image_url = response['data'][0]['url']
 
-# Load prompt templates from a YAML file. Prompts may or may not have placeholders for user inputs.
-
-
-
-# Define a function to fill in the prompt template
-# - Parse the prompt template to find placeholders and ask the user to fill them in
-# - Substitute the filled placeholders into the prompt template
-
-
-
-# Define a function to generate a GPT response to a given prompt
-# - Use OpenAI's Completion API to generate a response to the prompt
-# - Extract the GPT-generated answer from the API response
-    
-
-# Main program entry point
-# - Ask the user to select an action
-# - If the user selects "freetext", ask for a prompt
-# - Otherwise, fill in the prompt template for the selected task
-# - Generate a GPT response to the prompt and print it
+# Print the generated text
+print(image_url)
